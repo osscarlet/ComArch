e984171f11f787a8a371dbc94370df39b23ab25a
holyfin <holyfin@gmail.com>
2013-09-20 11:05:14 +0700

    แยก field ลงแต่ละ array

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,66 @@ import java.util.*;
 
 public class Main {
 	
+	public static void displayList(ArrayList<String> list) {
+		for (int j=0; j<list.size(); j++)
+			System.out.print(list.get(j)+" ");
+		System.out.println();
+		for (int j=0; j<list.size(); j++)
+			System.out.println("["+j+"] "+list.get(j));
+		System.out.println("Size: "+list.size());
+	}
+	
 	public static boolean isInst(String text) {
-		if (text.equals("and") || text.equals("nand") || text.equals("lw") || text.equals("sw") || text.equals("beq") || text.equals("jalr") || text.equals("halt") || text.equals("noop"))
+		if (text.equalsIgnoreCase("add") 
+				|| text.equalsIgnoreCase("nand") 
+				|| text.equalsIgnoreCase("lw") 
+				|| text.equalsIgnoreCase("sw") 
+				|| text.equalsIgnoreCase("beq") 
+				|| text.equalsIgnoreCase("jalr") 
+				|| text.equalsIgnoreCase("halt") 
+				|| text.equalsIgnoreCase("noop"))
 			return true;
 		else
 			return false;
 	}
+	
+	public static boolean isRtype(String text) {
+		if (text.equalsIgnoreCase("add"))
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean isItype(String text) {
+		if (text.equalsIgnoreCase("lw")
+				|| text.equalsIgnoreCase("sw")
+				|| text.equalsIgnoreCase("beq"))
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean isJtype(String text) {
+		if (text.equalsIgnoreCase("jalr"))
+			return true;
+		else return false;
+	}
+	
+	public static boolean isOtype(String text) {
+		if (text.equalsIgnoreCase("halt")
+				|| text.equalsIgnoreCase("noop")
+				)
+			return true;
+		else return false;
+	}
+	
+	public static boolean isFill(String text) {
+		if (text.equalsIgnoreCase(".fill"))
+			return true;
+		else return false;
+		
+	}
+	
 
 	public static void main(String[] args) throws FileNotFoundException {
 		//read from text file
@@ -19,43 +73,63 @@ public class Main {
 		currentDir += "/src/sample.txt";
         File file = new File(currentDir);
         Scanner sn = new Scanner(file);
-		int num = 0;
+
 		
 		while(sn.hasNextLine()) {
 			String line = sn.nextLine();
-			num++;
 			StringTokenizer strtok = new StringTokenizer(line); //create string tokenizer, slice by space
-			int i=0;	
+	
 			ArrayList<String> code = new ArrayList<String>();
 			while (strtok.hasMoreElements()) {
-				code.add((String) strtok.nextElement());
-				System.out.print(code.get(i)+" ");
-				i++;
+				code.add(strtok.nextElement().toString());
 			}
-
-			// 0 label
-			// 1 inst
-			// 2 field0
-			// 3 field1
-			// 4 field2
-			// 5 comment
-
-			int index;
-			if(isInst(code.get(0)))
-				index=0;
-			else
-				index=1;
 			
-			System.out.println();
-			System.out.print(code.get(index));
+			if(isInst(code.get(0))) {	
+				code.add(0, null);;
+				/*
+				for (int j=code.size()-1; j>0 ;j--) {
+					code.set(j, code.get(j-1));
+				}
+				code.set(0, null);
+				*/
+			}
 			
-
-			System.out.println();
+			if (isRtype(code.get(1)) || isItype(code.get(1))) {
+				String tmp = "";
+				while (code.size()>5) {
+					tmp += code.get(5) + " ";
+					code.remove(5);
+				}
+				code.add(tmp);
+			} else if (isJtype(code.get(1))) {
+				String tmp = "";
+				while (code.size()>4) {
+					tmp += code.get(4) + " ";
+					code.remove(4);
+				}
+				code.add(tmp);
+				
+			} else if (isOtype(code.get(1))) {
+				String tmp = "";
+				while (code.size()>2) {
+					tmp += code.get(2) + " ";
+					code.remove(2);
+				}
+				code.add(tmp);
+			} else if (isFill(code.get(1))) {
+				String tmp = "";
+				while (code.size()>3) {
+					tmp += code.get(3) + " ";
+					code.remove(3);
+				}
+				code.add(tmp);
+			}
 			
+			
+			
+			displayList(code);
+					
+			System.out.println();		
 		}
-	
-		
-
 	}
-
 }
